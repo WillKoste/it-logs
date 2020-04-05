ae24ff0774ea84fbbe897946e9958a63fd4457fd
Will Koste <willkoste@gmail.com>
2020-04-05 16:00:09 -0400

    Logs LogItem Preloader

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className="progress blue lightn-4">
+      <div className="indeterminate blue"></div>
+    </div>
+  )
+}
+
+export default Preloader;
