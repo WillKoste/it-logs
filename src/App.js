66d1d8f55275d3f5dcda7980bfdfbedca9bd41d8
Will Koste <willkoste@gmail.com>
2020-04-04 21:16:35 -0400

    SearchBar Logs LogItem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -9,9 +11,12 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <div className="App">
-      My APP :D
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
   );
 }
 
