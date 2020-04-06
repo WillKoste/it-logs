6dcabfe5ee477b586c24b62d316e1fa779b03b25
Will Koste <willkoste@gmail.com>
2020-04-06 11:39:34 -0400

    updateLog

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from './types';
 
 export const getLogs = () => async dispatch => {
@@ -67,6 +70,45 @@ export const deleteLog = (id) => async dispatch => {
       type: LOGS_ERROR,
       payload: err.response.data
     });
+  }
+}
+
+export const updateLog = (log) => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const data = await res.json()
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+}
+
+export const setCurrent = (log) => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  }
+}
+
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
   }
 }
 
